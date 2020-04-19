ec6a5aafb2a1a1b5704ebdb066faa93ddfdd3040
Parardha Kumar <parardhakumar@gmail.com>
2020-04-19 19:42:18 +0530

    Add Express

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+  res.render("index");
+})
+
+app.get("*", function(req, res){
+  res.render("404");
+})
+
+app.listen(process.env.PORT || 9999 , function(){
+  console.log("Server Running Successfully!");
+})
